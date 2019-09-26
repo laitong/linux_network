5db54000295c07aeca2a27ef39b25b8d4e0ec52d
laitong <49020241+laitong@users.noreply.github.com>
2019-09-26 14:51:39 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-echo -e "\n"
-clear
-echo -e "\n"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m Welcome to Desktop environment and Remote connection tool ! V2.0.2 \033[0m"
-echo "System requirements: CentOS 7+ / Debian 9+ / Ubuntu 18.04+ New installation"
-echo -e "\033[33m This shell is for the system: Ubuntu 18.04+, if you need to run on other systems, please refer to the following tutorial. \033[0m"
-echo "[Original] One-click installation of Desktop environment, RDP, Windows support for Linux"
-echo "URL：https://tech.cxthhhhh.com/linux/2018/08/07/original-one-click-installation-of-desktop-environment-rdp-windows-support-for-linux-en.html"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m It is recommended to use CentOS 7 installation, the following pure system is recommended. \033[0m"
-echo "Recommended system : [Original] Network one-click Reinstall CentOS 7 (Official, Pure, Safe, Efficient)"
-echo "URL : https://tech.cxthhhhh.com/linux/2018/07/30/original-network-one-click-reinstall-centos-7-official-pure-safe-efficient-en.html"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "from https://tech.cxthhhhh.com - 2018/09/10 - MeowLove"
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo Press any key to continue! Exit with 'Ctrl'+'C' !
+
 
 echo -e "\n"
 cd /root
